4a99a67b999d8a3d5793fb19c604e76f4549bca2
Nicholas Ewasiuk <ewasiuk.nick@gmail.com>
2021-10-19 18:34:30 -0700

    this is fun

--- index.js
+++ index.js
@@ -5,19 +5,36 @@ function getRandomInt(length) {
   return Math.floor(Math.random() * length); //The maximum is exclusive and the minimum is inclusive
 }
 
-let nameArray = [];
 
-for (let i = 0; i < 2; i++) {
+function gibberishFromArrays(obj, wordLengthSeed) {
+  let nameArray = [];
+
+  for (let i = 0; i < wordLengthSeed; i++) {
+    let numKeys = Object.keys(obj).length;
+    let randKey = getRandomInt(numKeys);
+    let key = Object.keys(obj)[randKey]
   
-  let numKeys = Object.keys(address).length;
+    let arrayIndex = getRandomInt(obj[key].length);
 
-  let randKey = getRandomInt(numKeys);
+    nameArray.push(obj[key][arrayIndex]);
+  }
 
-  let key = Object.keys(address)[randKey]
-
-  let arrayIndex = getRandomInt(address[key].length);
-
-  nameArray.push(address[key][arrayIndex]);
+  return nameArray.join("");
 }
 
-console.log(nameArray.join(""));
+function cityName(obj) {
+  let nameArray = [];
+
+  for (let key in obj) {
+    let arrayIndex = getRandomInt(obj[key].length);
+    nameArray.push(obj[key][arrayIndex]);
+  }
+
+  return nameArray.join("");
+}
+
+
+
+console.log(gibberishFromArrays(address, 8));
+
+console.log(cityName(address));
